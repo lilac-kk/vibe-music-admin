78f6decb3d4b52c77eda3e2fd023b4a19c61f43e
lilac-kk <184565509@qq.com>
2026-03-26 15:45:14 +0800

    Create types.ts

---
+++ src/views/blindbox/rarity/utils/types.ts
@@ -0,0 +1,15 @@
+interface FormItemProps {
+  /** 用于判断是`新增`还是`修改` */
+  title: string;
+  id?: number;
+  name: string;
+  weight: number;
+  color: string;
+  icon?: string;
+}
+
+interface FormProps {
+  formInline: FormItemProps;
+}
+
+export type { FormItemProps, FormProps };
